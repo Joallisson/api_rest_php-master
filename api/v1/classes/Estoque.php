01637936acbfc22015bbdfd33debf01a63fd3b4b
Joallisson <69011012+Joallisson@users.noreply.github.com>
2021-01-06 12:03:10 -0300

    CRIANDO A CLASSE ESTOQUE E O MÉTODO MOSTRAR

--- api/v1/classes/Estoque.php
+++ api/v1/classes/Estoque.php
@@ -1,5 +1,28 @@
 <?php
 
+class Estoque{ //CRIANDO A CLASSE
+
+	public static function mostrar() //CRIANDO O MÉTODO
+	{
+		$con = new PDO('mysql: host=localhost, dbname=estoque', 'root', ''); //CRIANDO A CONEXÃO COM O DB
+
+		$sql = "SELECT * FROM estoque ORDER BY id ASC"; //CRIANDO UM SCRIPM SQL E JOGANDO DENTRO DA VARIÁVEL
+		$sql = $con->prepare($sql); //PREPARANDO O SCRIPT SQL E PARA JOGAR NA VARIÁVEL
+		$sql->execute(); // EXECUTANDO O SCRIPT DA CONSULTA
+
+		$resultados = array(); //CRIANDO UM ARRAY
+
+		while ($row = $sql->fetch(PDO::FETCH_ASSOC)) { //ENQUANTO HOUVER RETORNO DE DADOS | fetch(): Retorna uma unica row da consulta E O PDO::FETCH_ASSOC SERVE PARA RETORNA SOMENTE 1 CAMPO NO RETORNO PARA A VARIÁVEL
+			$resultados[] = $row; //A VARIÁVEL $resultados  IRÁ RECEBER O RETORNO DOS DADOS DA VARIÁVEL $row
+		}
+
+		if (!$resultados) { //SENÃO TIVER RESULTADOS
+			throw new Exception("Nenhum produto no estoque!"); //EXIBE UMA MENSAGEM DE ERRO
+		}
+
+		return $resultados; //RETORNO DE DADOS
+	}
+}
 
 
 
